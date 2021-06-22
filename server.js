62821459855ab0715508518a253307c0d23b80b9
modou.diao <modou.diao@gmail.com>
2021-06-22 10:06:44 +0000

    change folder index

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/clinical_front'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/clinical_front/index.html'));
+res.sendFile(path.join(__dirname+'/clinical_front/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
